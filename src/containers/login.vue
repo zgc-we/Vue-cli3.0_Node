d850f55344110dcd6ab3380d467a5cd6593849bb
ludan <ludan1107743896@qq.com>
2019-08-16 17:53:09 +0800

    models添加

--- src/containers/login.vue
+++ src/containers/login.vue
@@ -1,10 +1,23 @@
 <template>
-    <div>login</div>
+    <div>
+        login{{count}}
+    </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
 export default {
-    
+    data() {
+        return {}
+    },
+    created: function() {
+        this.$store.dispatch({
+            type: 'fetch_login_init',
+        })
+    },
+    computed: mapState({
+        count: state => state.login.count,
+    })
 }
 </script>
 
