952b80b27bea4181275fb482eb84bfae9d600654
ludan <1107743896@qq.com>
2019-08-18 10:17:06 +0800

    dasboard.vue 首页面提交

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
+import './assets/css/base.css'
+import './assets/css/checkout.css'
+import './assets/css/login.css'
+import './assets/css/product.css'
+
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios);
 
